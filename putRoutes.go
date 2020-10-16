31c79d937b80e042818ec5afd7e1b4dff4740faf
vadimdoga <vadim.doga1@gmail.com>
2020-10-16 21:17:52 +0300

    structured into packages

--- putRoutes.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "net/http"
-
-// UpdateStoredProducts ...
-func UpdateStoredProducts(w http.ResponseWriter, r *http.Request) {
-	//*add quantity, change status using and id
-}
