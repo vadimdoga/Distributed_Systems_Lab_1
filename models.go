0eed8bf19b6101790ece2f88f33b62112fc44ea7
vadimdoga <vadim.doga1@gmail.com>
2020-10-16 12:01:21 +0300

    connected to mongodb, created models

---
+++ models.go
@@ -0,0 +1,18 @@
+package main
+
+import "time"
+
+// Products ...
+type Products struct {
+	ID             int       `json:"id"`
+	Title          string    `json:"title"`
+	StorageAddress string    `json:"storage_address"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Status         string    `json:"status"`
+}
+
+// MsgResponse ...
+type MsgResponse struct {
+	Message string `json:"message"`
+}
