4822123c4dcba01789f9f7ab863f15c7d5efc29c
vadimdoga <vadim.doga1@gmail.com>
2020-10-16 12:04:39 +0300

    encapsulated to JSONResponse and JSONError

---
+++ utils.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// JSONError ...
+func JSONError(w http.ResponseWriter, err interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(err)
+}
+
+// JSONResponse ...
+func JSONResponse(w http.ResponseWriter, msg string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	res := MsgResponse{Message: msg}
+	response, err := json.Marshal(res)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.Write(response)
+}
