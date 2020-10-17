da7eac76f4a04a4ea05bed9fa372e4a7ca405bb1
vadimdoga <vadim.doga1@gmail.com>
2020-10-17 12:21:23 +0300

    added new field

--- database/models.go
+++ database/models.go
@@ -4,9 +4,9 @@ import "time"
 
 // Products ...
 type Products struct {
-	ID             int       `json:"id"`
 	Title          string    `json:"title"`
 	StorageAddress string    `json:"storage_address"`
+	Quantity       int       `json:"quantity"`
 	CreatedAt      time.Time `json:"created_at"`
 	UpdatedAt      time.Time `json:"updated_at"`
 	Status         string    `json:"status"`
