5323e347fa5786b24a1b15f7e7f47e4c3ad3d729
vadimdoga <vadim.doga1@gmail.com>
2020-10-17 12:42:57 +0300

    moved finalize func to PUT

--- routes/get.go
+++ routes/get.go
@@ -6,9 +6,3 @@ import "net/http"
 func GetStoredProducts(w http.ResponseWriter, r *http.Request) {
 	//* get all products
 }
-
-// FinalizeStoredProducts ...
-func FinalizeStoredProducts(w http.ResponseWriter, r *http.Request) {
-	// *get with id
-	//* move to processing the process
-}
