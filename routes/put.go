39ed2ec612b1c2a4ba05d26b95aca571221ae74e
vadimdoga <vadim.doga1@gmail.com>
2020-10-17 12:43:31 +0300

    added finalized route, return statements

--- routes/put.go
+++ routes/put.go
@@ -29,12 +29,14 @@ func UpdateStoredProducts(w http.ResponseWriter, r *http.Request) {
 	objID, idErr := primitive.ObjectIDFromHex(productID)
 	if idErr != nil {
 		utils.JSONError(w, idErr, 404)
+		return
 	}
 
 	checkStatusField := utils.CheckStatus(objID, "building")
 
 	if checkStatusField == false {
 		utils.JSONError(w, "This obj is not in building stage", 400)
+		return
 	}
 
 	filter := bson.M{"_id": bson.M{"$eq": objID}}
@@ -54,9 +56,49 @@ func UpdateStoredProducts(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		utils.JSONError(w, err, 500)
+		return
 	}
 
 	utils.JSONResponse(w, "successfully updated!", productID, "building", 200)
+	return
+}
 
+// FinalizeStoredProducts ...
+func FinalizeStoredProducts(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	productID := vars["id"]
+
+	objID, idErr := primitive.ObjectIDFromHex(productID)
+	if idErr != nil {
+		utils.JSONError(w, idErr, 404)
+		return
+	}
+
+	checkStatusField := utils.CheckStatus(objID, "building")
+	if checkStatusField == false {
+		utils.JSONError(w, "This obj is not in building stage", 400)
+		return
+	}
+
+	filter := bson.M{"_id": bson.M{"$eq": objID}}
+
+	update := bson.M{
+		"$set": bson.M{
+			"status": "processing",
+		},
+	}
+
+	_, err := dtb.ProductCollection.UpdateOne(
+		dtb.Ctx,
+		filter,
+		update,
+	)
+
+	if err != nil {
+		utils.JSONError(w, err, 500)
+		return
+	}
+
+	utils.JSONResponse(w, "successfully finalized!", productID, "processing", 200)
 	return
 }
